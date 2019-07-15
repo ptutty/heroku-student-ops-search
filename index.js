72f041c11a00d9ab1a0d6bb307600748548e640c
philip tutty <p.tutty@warwick.ac.uk>
2019-07-15 15:46:47 +0100

    added test routes

--- index.js
+++ index.js
@@ -5,17 +5,15 @@ const fetch = require("node-fetch");
 const fs = require('fs');
 const del = require('del');
 const searchConfig = require('./config/searchConfig');
-const searchApp = require('./app/searchApp');
+//const searchApp = require('./app/searchApp');
 const PORT = process.env.PORT || 5000
+
+console.log(searchConfig.fileName);
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/create', (req, res) => {
-    let cleanDir = searchApp.cleanDir();
-    console.log(cleanDir);
-    res.send();
-  })
+  .get('/help', (req, res) => res.render('pages/help'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
